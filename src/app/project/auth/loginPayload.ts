a37488e875a1b3c343cac6fafce9d84c51b3dd1e
ArseniouSoftwareConstructions <arseniouconstructions@gmail.com>
2024-06-24 23:37:53 +0300

    2nd Commint - Cleaning Code

--- src/app/project/auth/loginPayload.ts
+++ src/app/project/auth/loginPayload.ts
@@ -2,7 +2,7 @@ export class LoginPayload {
   email: string;
   password: string;
   constructor() {
-    this.email = 'trungk18@gmail.com';
+    this.email = 'chris0arseniou@gmail.com';
     this.password = `${new Date().getTime()}`;
   }
 }
